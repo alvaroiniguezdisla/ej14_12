f292e36afe783bf26d82d102d3be790f7edc5ee3
Álvaro <ainiguezdislaa@gmail.com>
2023-02-27 11:34:21 +0100

    ej terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,20 @@
-package aplicacion;public class Principal {
+package aplicacion;
+import java.util.ArrayList;
+import  java.util.List;
+import dominio.ej14_12;
+public class Principal {
+    public static void main(String[] args) {
+        List<Integer> lista = new ArrayList<>();
+        lista.add(1);
+        lista.add(2);
+        lista.add(3);
+        lista.add(4);
+        lista.add(5);
+        lista.add(6);
+        lista.add(7);
+        lista.add(8);
+        lista.add(9);
+        lista.add(10);
+        System.out.println(ej14_12.por2MayorQue6(lista));
+    }
 }
--- src/dominio/ej14_12.java
+++ src/dominio/ej14_12.java
@@ -1,2 +1,11 @@
-package dominio;public class ej14_12 {
+package dominio;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+
+public class ej14_12 {
+    public static List<Integer> por2MayorQue6(List<Integer> lista){
+        return Stream.iterate(0,i->i+1).limit(lista.size()).map(i->lista.get(i)*2).filter(i->i>6).collect(Collectors.toCollection(ArrayList::new));
+    }
 }
